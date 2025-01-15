6f6625e3e02dc792ad3eb44010a20f67310d5199
Daksh Mahajan <dakshmahajan0110@gmail.com>
2025-01-15 12:22:42 +0530

    fixed some speelings of routes

--- swagger.js
+++ swagger.js
@@ -10,7 +10,7 @@ const doc = {
 };
 
 const outputFile = "./swagger-output.json";
-const endpointsFiles = ["./messageRoutes.ts", "./routes.ts"];
+const endpointsFiles = ["./messageRoutes.ts", "./userRoutes.ts"];
 
 swaggerAutogen(outputFile, endpointsFiles).then(() => {
   console.log("Swagger documentation generated!");
